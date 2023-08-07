ae069c9cdf997a4e02156d8798ce7dcaaae0dcd5
LeoArs06 <leoars03@gmail.com>
2023-08-07 11:54:58 +0200

    Initial commit

---
+++ src/main/java/ares06/craftystarter/Craftystarter.java
@@ -0,0 +1,17 @@
+package ares06.craftystarter;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Craftystarter extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
